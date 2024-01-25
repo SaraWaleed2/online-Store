4d6a521d2ebe09952360bb0ea0922d193eecfa70
Sara <sarawaleed8479@gmail.com>
2024-01-25 18:24:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var largeImage=document.getElementById("largeImage");
+var smallImage=document.getElementsByClassName("small-image");
+
+smallImage[0].onclick = function(){
+    largeImage.src =smallImage[0].src;
+}
+smallImage[1].onclick = function(){
+    largeImage.src =smallImage[1].src;
+}
+smallImage[2].onclick = function(){
+    largeImage.src =smallImage[2].src;
+}
+smallImage[3].onclick = function(){
+    largeImage.src =smallImage[3].src;
+}
+smallImage[4].onclick = function(){
+    largeImage.src =smallImage[4].src;
+}
